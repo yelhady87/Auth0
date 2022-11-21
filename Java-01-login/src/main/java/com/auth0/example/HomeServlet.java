25bdc0408762e2afc63d69aa1b367665ec32c108
Yehia ElHady <yehia.elhady@incorta.com>
2022-11-21 22:20:45 +0200

    Auth0 Servlet Sample

---
+++ Java-01-login/src/main/java/com/auth0/example/HomeServlet.java
@@ -0,0 +1,31 @@
+package com.auth0.example;
+
+import com.auth0.SessionUtils;
+import com.auth0.jwt.algorithms.Algorithm;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/portal/home"})
+public class HomeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        final String accessToken = (String) SessionUtils.get(req, "accessToken");
+        final String idToken = (String) SessionUtils.get(req, "idToken");
+        if (accessToken != null) {
+            req.setAttribute("userId", accessToken);
+        } else if (idToken != null) {
+            req.setAttribute("userId", idToken);
+        }
+
+//        Algorithm.
+
+
+        req.getRequestDispatcher("/WEB-INF/jsp/home.jsp").forward(req, res);
+    }
+}
